69a96f6c26b2c40c147cd3ed696a5d7e577c6ff0
Igor Pilja <10416207+dzigy@users.noreply.github.com>
2025-02-11 17:54:12 -0800

    Data Preparation and Exploration steps

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
 import pandas as pd
 import numpy as np
 
+
 originalData =  pd.read_csv('Employee Turnover Dataset.csv')
 df = pd.DataFrame(originalData)
 
-# remove whitespace from the columnames of a DataFrame
+# check data type for each column
+print('Check data types')
+print(df.dtypes)
+
+# remove whitespace from the column names of a DataFrame
 df.columns = df.columns.str.strip()
 print('Print data')
 print(df)
 
+################## DUPLICATES###################
 # check for duplicates
 print('Check for duplicates')
 print(df.duplicated())
 duplicates = df.duplicated().sum()
-print('Number of duplicates: ', duplicates)
+print('Number of duplicate entries: ', duplicates, 'rows')
+
 
 # remove duplicates
 df=df.drop_duplicates();
-
+#################################################
+################## MISSING VALUES################
 # check for missing values
 missingValues = df.isnull().sum()
-print('Number of missing values: ', missingValues)
+print('Number of missing values per column: ')
+print(missingValues)
+print("\n")
 
-#print(df.columns)
-print('Check data types')
-print(df.dtypes)
 
+# Handling missing values
+# for categorical data - adding most frequent category
+# for numerical data - replace missing value with median value
+for column in df.columns:
+    if df[column].dtype == 'object':  # Categorical columns
+        df[column].fillna(df[column].mode()[0], inplace=True)
+    else:  # Numeric columns
+        df[column].fillna(df[column].median(), inplace=True)
+
+#################################################
+############ INCONSISTENT ENTRIES################
+
+# check for inconsistent entries - code below will display the unique values in each categorical column.
+for column in df.select_dtypes(include=['object']).columns:
+    print(f"Unique values in {column}: {df[column].unique()}")
+
+# fix inconstency
+df['Gender'] = df['Gender'].str.lower()  # Convert all entries to lowercase
+df['CompensationType'] = df['CompensationType'].str.capitalize()  # Capitalize first letter
+df['MaritalStatus'] = df['MaritalStatus'].str.capitalize()  # Capitalize first letter
+
+#################################################
+############ FORMATTING ERRORS ##################
 
 # code that counts how many rows in the 'HourlyRate' column contain invalid characters
 # (anything other than digits, a decimal point, or a hyphen).
@@ -37,55 +67,25 @@ df['HourlyRate'] = pd.to_numeric(df['HourlyRate'])
 # check if data type is correct
 print('HourlyRate data type: ',df['HourlyRate'].dtype)
 
-# handling missing values for categorical data by adding most frequent category
-df['Turnover'].fillna(df['Turnover'].mode()[0], inplace=True)
-df['CompensationType'].fillna(df['CompensationType'].mode()[0], inplace=True)
-df['JobRoleArea'].fillna(df['JobRoleArea'].mode()[0], inplace=True)
-df['Gender'].fillna(df['Gender'].mode()[0], inplace=True)
-df['MaritalStatus'].fillna(df['MaritalStatus'].mode()[0], inplace=True)
-df['PaycheckMethod'].fillna(df['PaycheckMethod'].mode()[0], inplace=True)
-df['TextMessageOptIn'].fillna(df['TextMessageOptIn'].mode()[0], inplace=True)
+#################################################
+################## OUTLIERS #####################
 
-# handling missing values for numerical data by replace missing value with mean value
-df['EmployeeNumber'].fillna(df['EmployeeNumber'].mean(), inplace=True)
-df['HourlyRate'].fillna(df['HourlyRate'].mean(), inplace=True)
-df['Age'].fillna(df['Age'].mean(), inplace=True)
-df['Tenure'].fillna(df['Tenure'].mean(), inplace=True)
-df['HoursWeekly'].fillna(df['HoursWeekly'].mean(), inplace=True)
-df['AnnualSalary'].fillna(df['AnnualSalary'].mean(), inplace=True)
-df['DrivingCommuterDistance'].fillna(df['DrivingCommuterDistance'].mean(), inplace=True)
-df['NumCompaniesPreviouslyWorked'].fillna(df['NumCompaniesPreviouslyWorked'].mean(), inplace=True)
-df['AnnualProfessionalDevHrs'].fillna(df['AnnualProfessionalDevHrs'].mean(), inplace=True)
-
-# check for inconsistent entries - code below will display the unique values in each categorical column.
-for column in df.select_dtypes(include=['object']).columns:
-    print(f"Unique values in {column}: {df[column].unique()}")
-
-# fix inconstency
-df['Gender'] = df['Gender'].str.lower()  # Convert all entries to lowercase
-df['CompensationType'] = df['CompensationType'].str.capitalize()  # Capitalize first letter
-df['MaritalStatus'] = df['MaritalStatus'].str.capitalize()  # Capitalize first letter
-
-
-# function that checks for outliers
-# IQR: Any value below Q1 - 1.5IQR or above Q3 + 1.5IQR is considered an outlier.
-def check_outliers(df):
-    outliers = {}
-    for column in df.select_dtypes(include=['int64', 'float64']).columns:
-        # Using IQR to detect outliers
+# Handling outliers - using IQR to remove extreme outliers
+#Using the IQR method, we calculate the lower and upper bounds and flag any values outside this range as outliers.
+outliers = {}
+for column in df.select_dtypes(include=['int64', 'float64']).columns:
         Q1 = df[column].quantile(0.25)
         Q3 = df[column].quantile(0.75)
         IQR = Q3 - Q1
         outlier_condition = (df[column] < (Q1 - 1.5 * IQR)) | (df[column] > (Q3 + 1.5 * IQR))
         outliers[column] = df[column][outlier_condition]
-    return outliers
+        # capping outliers
+        df[column] = df[column].clip(lower=Q1 - 1.5 * IQR, upper=Q3 + 1.5 * IQR)
 
-print("5. Outliers:")
-print("Outliers found in numeric columns:")
-for column, outlier_values in check_outliers(df).items():
-    print(f"{column}: {outlier_values}")
-print("\n")
 
+# outliers are valid just very different values so i am not removing them
+#################################################
+########### EXPORT CLEANED DATASET ##############
 
 # export cleaned data
 df.to_csv("CleanData.csv", index=False)
